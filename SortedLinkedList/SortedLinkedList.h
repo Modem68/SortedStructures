e5b63b9a62d49257fde8c6bcc034fd95eb14bf69
Simon Ayzman <simonayzman@gmail.com>
2016-03-22 11:53:34 -0400

    Added comments about complexity

--- SortedLinkedList/SortedLinkedList.h
+++ SortedLinkedList/SortedLinkedList.h
@@ -5,30 +5,36 @@
 #include <list>
 #include <algorithm>
 
+/*
+    This class implements a sorted doubly linked list 
+    using the STL list as its underlying container
+*/
 template<class T>
 class SortedLinkedList{
 public:
     SortedLinkedList() {}
 
+    // O(1) operations
     unsigned int size() { return linkedList.size(); }
     bool empty() { return linkedList.empty(); }
-    void clear() { linkedList.clear(); }
 
     T& front() { return linkedList.front(); }
     T& back() { return linkedList.back(); }
-    T& at(unsigned int pos) { auto it = linkedList.begin(); std::advance(it,pos); return *it; }
 
     void push_front(const T& data);
     void push_back(const T& data);
     T& pop_front() { T& data = back(); linkedList.pop_back(); return data; }
     T& pop_back() { T& data = back(); linkedList.pop_back(); return data; }
-    
-    T& remove(unsigned int pos) { T& data = at(pos); linkedList.erase(linkedList.begin()+pos); return data; }
 
-    void insert(const T& data);
-
+    // O(n) operations
+    void clear() { linkedList.clear(); }
+    T& at(unsigned int pos) { auto it = linkedList.begin(); std::advance(it,pos); return *it; }
     unsigned int count(const T& data);
     void display();
+
+    void insert(const T& data);
+    T& remove(unsigned int pos) { T& data = at(pos); linkedList.erase(linkedList.begin()+pos); return data; }
+
     
 private:
     std::list<T> linkedList;
--- SortedVector/SortedVector.h
+++ SortedVector/SortedVector.h
@@ -4,26 +4,32 @@
 #include <iostream>
 #include <vector>
 
+/*
+    This class implements a sorted vector using 
+    the STL vector as its underlying container
+*/
 template<class T>
 class SortedVector{
 public:
     SortedVector() { vec.reserve(50); }
 
+    // O(1) operations
     unsigned int capacity() { return vec.capacity(); }
     unsigned int size() { return vec.size(); }
     bool empty() { return vec.empty(); }
-    void clear() { vec.clear(); }
 
+    T& at(unsigned int pos) { return vec.at(pos); }
     T& front() { return vec.front(); }
     T& back() { return vec.back(); }
-    T& at(unsigned int pos) { return vec.at(pos); }
     T& pop_back() { T& data = back(); vec.pop_back(); return data; }
-    T& remove(unsigned int pos) { T& data = at(pos); vec.erase(vec.begin()+pos); return data; }
 
-    void insert(const T& data);
-
+    // O(n) operations
+    void clear() { vec.clear(); }
     unsigned int count(const T& data);
     void display();
+    
+    void insert(const T& data);
+    T& remove(unsigned int pos) { T& data = at(pos); vec.erase(vec.begin()+pos); return data; }
     
 private:
     std::vector<T> vec;
